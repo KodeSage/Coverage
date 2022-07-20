cfc3e03930f0da1790e86c9196b8e505b08b0723
James Harrison <somtochukwujames2000@gmail.com>
2022-07-20 23:04:50 +0100

    feat: added compound protocol

--- coveragesmartcontract/scripts/deploy.js
+++ coveragesmartcontract/scripts/deploy.js
@@ -1,28 +1,24 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
+
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  // const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  // const lockedAmount = hre.ethers.utils.parseEther("1");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  //  0xFEe1C02B91Fd7e846962E9e9336E4D7082420E11
+const cUSDC = "0x4a92e71227d294f041bd82dd8f78591b75140d63";
+  const Coverage = await hre.ethers.getContractFactory("Coverage");
+  const lock = await Coverage.deploy(cUSDC );
 
   await lock.deployed();
 
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  console.log("Coverage deployed to:", lock.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
